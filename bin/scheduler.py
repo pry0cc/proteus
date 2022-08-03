763ca272889ec721ecec71aea20f546d2f3b40f1
pry <you@example.com>
2022-08-03 22:28:15 +0000

    updated scanner/scheduler to be dnsx

--- bin/scheduler.py
+++ bin/scheduler.py
@@ -7,10 +7,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+r.rpush('queue','bounty')    
+
 scheduler = BlockingScheduler()
 @scheduler.scheduled_job(IntervalTrigger(hours=5))
 def queue_job():
     print('queuing!')
-    r.rpush('queue','army')    
+    r.rpush('queue','bounty')    
 
 scheduler.start()
